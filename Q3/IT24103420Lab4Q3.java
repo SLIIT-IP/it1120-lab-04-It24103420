7f5bc093c74c3be3d680290c257efa48dd704006
It24103420 <it24103420@my.sliit.lk>
2024-08-18 19:47:51 +0530

    Add files via upload

---
+++ Q3/IT24103420Lab4Q3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT24103420Lab4Q3
+{
+	public static void main(String[] args)
+		{
+		   Scanner scanner=new Scanner(System.in);
+		   
+		   System.out.print("Enter a number: ");
+		   int number=scanner.nextInt();
+		   
+		   String result=(number>0) ? "Positive":
+						 (number<0) ? "Negative":
+						 "Zero";
+						
+	       System.out.println("The number is: " + result);
+		   }
+}
